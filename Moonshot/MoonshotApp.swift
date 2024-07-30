cda9da3a14c00a45da5b88a7d36e1234bb796875
Silvia Chang <sunny1218.chang@gmail.com>
2024-07-30 13:56:54 +1000

    Initial Commit

---
+++ Moonshot/MoonshotApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoonshotApp.swift
+//  Moonshot
+//
+//  Created by Sunny on 2024/7/30.
+//
+
+import SwiftUI
+
+@main
+struct MoonshotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
